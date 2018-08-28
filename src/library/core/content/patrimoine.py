51689e9a97f4018dd2344d8d5c42d2b97582cef5
Nicolas Demonte <nicolas@affinitic.be>
2018-08-28 12:15:05 +0200

    Store Patrimoine date field as text

--- src/library/core/content/patrimoine.py
+++ src/library/core/content/patrimoine.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from plone.dexterity.content import Container
 from plone.dexterity.browser import view
 from plone.supermodel import model
+from re import match
 from zope.interface import implementer
-from plone.autoform import directives as form
-
-from library.core.widget.textdate import TextDateFieldWidget
+from zope.interface import Invalid
+from z3c.form.validator import SimpleFieldValidator
 
 
 class IPatrimoine(model.Schema):
@@ -13,7 +14,32 @@ class IPatrimoine(model.Schema):
     """
 
     model.load('patrimoine.xml')
-    form.widget('date', TextDateFieldWidget)
+
+
+class DateValidator(SimpleFieldValidator):
+
+    regex_formats = {
+        '\d{8}$': '%d%m%Y',
+        '\d{6}$': '%m%Y',
+        '\d{4}$': '%Y',
+        '\d{1,2}/\d{1,2}/\d{4}$': '%d/%m/%Y',
+        '\d{1,2}/\d{4}$': '%m/%Y',
+        '\d{1,2}-\d{1,2}-\d{4}$': '%d-%m-%Y',
+        '\d{1,2}-\d{4}$': '%m-%Y',
+    }
+
+    def validate(self, value, force=False):
+        super(DateValidator, self).validate(value, force)
+        if value:
+            stripped = value.strip()
+            for regex, datetime_format in self.regex_formats.items():
+                if match(regex, stripped):
+                    try:
+                        test = datetime.strptime(stripped, datetime_format).date()
+                        return True
+                    except ValueError as e:
+                        raise Invalid(u'Date invalide')
+            raise Invalid(u'Format d\'encodage non reconnu (jour/mois/année, mois/année ou année)')
 
 
 @implementer(IPatrimoine)
