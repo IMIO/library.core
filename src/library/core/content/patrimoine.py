29515d1d2ef6cadc2fe89f8e4a5f00a7e36cbbaf
Nicolas Demonte <nicolas@affinitic.be>
2018-09-25 15:42:50 +0200

    Index text fields in Patrimoine for Full text search

--- src/library/core/content/patrimoine.py
+++ src/library/core/content/patrimoine.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+from plone.app.textfield.value import IRichTextValue
 from plone.autoform import directives as form
 from plone.dexterity.content import Container
 from plone.dexterity.browser import view
+from plone.dexterity.utils import iterSchemata
+from plone.indexer.decorator import indexer
 from plone.supermodel import model
 from re import match
 from zope.interface import implementer
 from zope.interface import Invalid
+from zope.schema import getFields
 from z3c.form.validator import SimpleFieldValidator
+from Products.CMFPlone.utils import safe_unicode
+from Products.CMFPlone.utils import getToolByName
 
 from library.core.widget.textdate import TextDateFieldWidget
 
@@ -55,3 +61,35 @@ class Patrimoine(Container):
 class PatrimoineView(view.DefaultView):
     """
     """
+
+
+@indexer(IPatrimoine)
+def searchabletext_patrimoine(object, **kw):
+    """
+    Indexes every text and rich text field (+ keywords) in Patrimoine objects,
+    making them available in Full Text Search.
+    """
+    result = []
+    subjects = getattr(object, 'subject', None)
+    if type(subjects) is tuple:
+        text = ' '.join([safe_unicode(s)
+                         for s in subjects
+                         if type(safe_unicode(s) is unicode)])
+        result.append(text.encode('utf-8'))
+
+    for schemata in iterSchemata(object):
+        for field_name, field_type in getFields(schemata).items():
+            value = getattr(object, field_name, None)
+            if type(value) is unicode:
+                text = safe_unicode(value).encode('utf-8')
+                result.append(text)
+            elif IRichTextValue.providedBy(value):
+                transforms = getToolByName(object, 'portal_transforms')
+                text = transforms.convertTo(
+                    'text/plain',
+                    safe_unicode(value.raw).encode('utf-8'),
+                    mimetype=value.mimeType,
+                ).getData().strip()
+                result.append(text)
+
+    return ' '.join(result)
