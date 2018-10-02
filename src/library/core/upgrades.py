c1294439fb639cb8d54dcca60b2f8d4c72628278
Nicolas Demonte <nicolas@affinitic.be>
2018-10-02 14:44:34 +0200

    Upgrade step to purge Patrimoine lead image scales

--- src/library/core/upgrades.py
+++ src/library/core/upgrades.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
 from plone.app.upgrade.utils import loadMigrationProfile
+from zope.annotation.interfaces import IAnnotations
+import logging
 
 
 def reload_gs_profile(context):
@@ -7,3 +10,15 @@ def reload_gs_profile(context):
         context,
         'profile-library.core:default',
     )
+
+
+def purge_patrimoine_scales(context):
+    logger = logging.getLogger('library.core')
+    pc = getToolByName(context, 'portal_catalog')
+    for brain in pc(portal_type='patrimoine'):
+        patrimoine = brain.getObject()
+        annotations = IAnnotations(patrimoine)
+        if 'plone.scale' in annotations:
+            del annotations['plone.scale']
+            logger.info('purged {0}'.format(patrimoine.id))
+    logger.info('scales purge over.')
