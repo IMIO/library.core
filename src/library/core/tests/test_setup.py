7233eaef1ed5351f7b5925892ccea3526c781a38
cboulanger <christophe.boulanger@imio.be>
2023-06-06 09:31:57 +0200

    Migrate to Plone6

--- src/library/core/tests/test_setup.py
+++ src/library/core/tests/test_setup.py
@@ -21,7 +21,7 @@ class TestSetup(unittest.TestCase):
 
     def test_product_installed(self):
         """Test if library.core is installed."""
-        self.assertTrue(self.installer.isProductInstalled("library.core"))
+        self.assertTrue(self.installer.is_product_installed("library.core"))
 
     def test_browserlayer(self):
         """Test that ILibraryCoreLayer is registered."""
@@ -40,12 +40,12 @@ class TestUninstall(unittest.TestCase):
         self.installer = get_installer(self.portal, self.layer["request"])
         roles_before = api.user.get_roles(TEST_USER_ID)
         setRoles(self.portal, TEST_USER_ID, ["Manager"])
-        self.installer.uninstallProducts(["library.core"])
+        self.installer.uninstall_product("library.core")
         setRoles(self.portal, TEST_USER_ID, roles_before)
 
     def test_product_uninstalled(self):
         """Test if library.core is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled("library.core"))
+        self.assertFalse(self.installer.is_product_installed("library.policy"))
 
     def test_browserlayer_removed(self):
         """Test that ILibraryCoreLayer is removed."""
