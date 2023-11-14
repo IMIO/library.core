63e47cbcc1d2bebe6b45444ce5e3aaeec756c98d
cboulanger <christophe.boulanger@imio.be>
2023-11-14 13:43:46 +0100

    Fix buildout and tests

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
     install_requires=[
         # -*- Extra requirements: -*-
         "plone.api>=1.8.4",
+        "plone.restapi",
         "Products.GenericSetup>=1.8.2",
         "setuptools",
         "z3c.jbot",
@@ -52,6 +53,7 @@ setup(
         "collective.taxonomy",
         "collective.z3cform.select2",
         "collective.faceted.map",
+        "iaweb.mosaic",
     ],
     extras_require={
         "test": [
