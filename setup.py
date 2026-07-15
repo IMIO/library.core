76e7fea8516f64078ef560dea981f701e62dac97
Benoît Suttor <ben.suttor@gmail.com>
2026-07-15 10:27:40 +0200

    Back to development: 3.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = "\n\n".join(
 
 setup(
     name="bibliotheca.core",
-    version="3.0.1",
+    version="3.0.2.dev0",
     description="Core package for Bibliotheca",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
